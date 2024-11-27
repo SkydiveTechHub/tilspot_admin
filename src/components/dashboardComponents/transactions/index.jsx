ac96918bf6cd15d5b400607867071e021cd3100f
Michael Adelola <mchladelola@gmail.com>
2024-11-27 13:47:10 +0100

    updated

--- src/components/dashboardComponents/transactions/index.jsx
+++ src/components/dashboardComponents/transactions/index.jsx
@@ -1,9 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Menu, Dropdown, Space, Table } from 'antd';
 import { EyeFilled, MoreOutlined } from '@ant-design/icons';
-import { Trash } from 'iconsax-react';
+import { MenuBoard, Trash } from 'iconsax-react';
 
 const TransactionsTable = ({ columns, data, actionMenu, noAction, handleDelete, handleView }) => {
+  const items = [
+    {
+      key: '1',
+      label: 'View',
+      extra: '⌘P',
+    },
+    {
+      key: '2',
+      label: 'Edit',
+      extra: '⌘B',
+    },
+    {
+      key: '3',
+      label: 'Enable',
+      // icon: <SettingOutlined />,
+      extra: '⌘S',
+    },
+    {
+      key: '4',
+      label: 'Delete',
+      // icon: <SettingOutlined />,
+      extra: '⌘S',
+    },
+  ];
 
   const usable_column =  [
     ...columns,
@@ -13,15 +37,26 @@ const TransactionsTable = ({ columns, data, actionMenu, noAction, handleDelete, 
       render: (_, record) => {
 
         return (
-            <div className=' flex gap-4'>
-              {
-                handleView && <button onClick={handleView}><img src="/images/eyes.png" alt="" /></button>
-              }
-              {
-                handleDelete && <button onClick={handleDelete}><img src="/images/bin.png" alt="" /></button>
-              }
+          <Dropdown
+            menu={{
+              items,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                <MenuBoard />
+              </Space>
+            </a>
+          </Dropdown>
+            // <div className=' flex gap-4'>
+            //   {
+            //     handleView && <button onClick={handleView}><img src="/images/eyes.png" alt="" /></button>
+            //   }
+            //   {
+            //     handleDelete && <button onClick={handleDelete}><img src="/images/bin.png" alt="" /></button>
+            //   }
               
-            </div>
+            // </div>
         );
       },
     },
--- src/components/shared/Modals/football/AddFootballProvider.jsx
+++ src/components/shared/Modals/football/AddFootballProvider.jsx
@@ -4,9 +4,11 @@ import { AuthButton } from '../../button';
 import useForm from '../../../../hooks/useForm';
 import { Modal } from 'antd';
 import ConfirmModal from '../ConfirmModal';
-import { GrayText } from '../../typograph';
+import { GrayText, Label } from '../../typograph';
 import SuccessModal from '../SuccessModal';
-// import SelectPlanModal from '../SelectPlanModal';
+import { DatePicker, Space } from 'antd';
+
+
 
 const initialState = {
   instance_name: '',
@@ -17,7 +19,7 @@ const AddFootballTicketProvider = ({ title, openModal, handleOk, handleCancel })
   const [active, setActive] = useState(false);
   const [more, setMore] = useState(false);
   const [secondModalOpen, setSecondModalOpen] = useState(false);
-
+  const [ticketType, setTicketType] = useState([{ name: "", price: ""}]);
   const { values, handleChange, resetForm, errors } = useForm(initialState);
 
   const handleSubmit = (e) => {
@@ -33,8 +35,19 @@ const AddFootballTicketProvider = ({ title, openModal, handleOk, handleCancel })
 
   useEffect(() => {
     validate();
-  }, [values]); // Re-run validation when `values` change
+  }, [values]); 
 
+    const IncreasePlanCount = () => {
+      setTicketType([...ticketType, { name: "", price: ""}]);
+  };
+
+  const DecreasePlanCount = (index) => {
+          setTicketType((prevItems) => prevItems.filter((_, i) => i !== index));
+
+  };
+  const onOk = (value) => {
+    console.log('onOk: ', value);
+  };
   return (
     <>
       <SuccessModal
@@ -52,8 +65,57 @@ const AddFootballTicketProvider = ({ title, openModal, handleOk, handleCancel })
         onCancel={handleCancel}
       >
         <form className='mt-6 space-y-6' onSubmit={handleSubmit}>
+          <div className='grid grid-cols-2 gap-6'>
+            <FormInput
+              label="League Type"
+              type="text"
+              name="instance_name"
+              value={values.instance_name}
+              onChange={handleChange}
+              placeholder="Enter provider name"
+              error={errors?.instance_name}
+    
+            />
+            <div className='relative w-full'>
+              <Label text={'Date & Time'}/>
+              <DatePicker
+                showTime
+                onChange={(value, dateString) => {
+                  console.log('Selected Time: ', value);
+                  console.log('Formatted Selected Time: ', dateString);
+                }}
+                onOk={onOk}
+              />
+            </div>
+                       
+          </div>
+
+          <div className='flex items-center justify-between gap-3'>
+            <FormInput
+              label="Team A"
+              type="text"
+              name="instance_name"
+              value={values.instance_name}
+              onChange={handleChange}
+              placeholder="Enter provider name"
+              error={errors?.instance_name}
+    
+            />
+            <span>-</span>
+            <FormInput
+              label="Team B"
+              type="text"
+              name="instance_name"
+              value={values.instance_name}
+              onChange={handleChange}
+              placeholder="Enter provider name"
+              error={errors?.instance_name}
+    
+            />
+          </div>
+
           <FormInput
-            label="Provider Name"
+            label="Stadium"
             type="text"
             name="instance_name"
             value={values.instance_name}
@@ -62,6 +124,84 @@ const AddFootballTicketProvider = ({ title, openModal, handleOk, handleCancel })
             error={errors?.instance_name}
   
           />
+
+          <FormInput
+            label="Amount"
+            type="text"
+            name="instance_name"
+            value={values.instance_name}
+            onChange={handleChange}
+            placeholder="Enter provider amount"
+            error={errors?.instance_name}
+          />
+
+<div className='space-y-4'>
+            {
+              ticketType.map((i, id)=>{
+                return(
+                  <div className='flex items-center gap-4'>
+
+                      <div className='grid gap-3 grid-cols-3'>
+                          <FormInput
+                            label="Ticket Name"
+                            type="text"
+                            name="instance_name"
+                            value={values.instance_name}
+                            onChange={handleChange}
+                            placeholder="Enter plan name"
+                            error={errors?.instance_name}
+                  
+                          />
+                          <FormInput
+                            label="Max Seat No"
+                            type="text"
+                            name="instance_name"
+                            value={values.instance_name}
+                            onChange={handleChange}
+                            placeholder="Enter plan price"
+                            error={errors?.instance_name}
+                  
+                          />     
+                          <FormInput
+                            label="Price per Ticket"
+                            type="text"
+                            name="instance_name"
+                            value={values.instance_name}
+                            onChange={handleChange}
+                            placeholder="Enter plan price"
+                            error={errors?.instance_name}
+                  
+                          />     
+                                        
+                        </div>                                    
+                        {ticketType.length > 1 && (
+                                <button
+                                    type="button"
+                                    className="font-[400] text-[red] font-Int text-[14px]"
+                                    onClick={()=>DecreasePlanCount(id)}
+                                >
+                                    <img src="/images/bin.png" alt="" />
+                                </button>
+                        
+                        )}
+                  </div>
+
+                )
+              })
+            }
+
+          </div>
+
+          <div className="flex justify-end w-full py-4">
+                            <button
+                                type="button"
+                                className="font-[500] text-[green] font-Int text-[14px]"
+                                onClick={IncreasePlanCount}
+                            >
+                                Add Ticket Type
+                            </button>
+                        </div>
+
           {/* <FormInput
             label=""
             type="select"
